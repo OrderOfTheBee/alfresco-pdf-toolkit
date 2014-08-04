3e9439a6f89f8531cafad238fc0353feffe21946
Nathan McMinn <nathan.mcminn@alfresco.com>
2014-08-04 00:57:55 -0500

    Supporting code and config for PDF/A convert
    Added:
    action icon
    form config
    module definition
    action bean
    archive level constraint

--- pdf-toolkit-repo/src/main/java/org/alfresco/extension/pdftoolkit/repo/action/executer/PDFConvertToArchivableActionExecuter.java
+++ pdf-toolkit-repo/src/main/java/org/alfresco/extension/pdftoolkit/repo/action/executer/PDFConvertToArchivableActionExecuter.java
@@ -1,7 +1,13 @@
 package org.alfresco.extension.pdftoolkit.repo.action.executer;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import net.sf.jooreports.converter.DocumentFamily;
+import net.sf.jooreports.converter.DocumentFormat;
+
+import org.alfresco.extension.pdftoolkit.constraints.MapConstraint;
 import org.alfresco.repo.action.ParameterDefinitionImpl;
 import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ParameterDefinition;
@@ -10,22 +16,26 @@ import org.alfresco.service.cmr.repository.NodeRef;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class PDFConvertToArchivableActionExecuter extends BasePDFActionExecuter {
+public class PDFConvertToArchivableActionExecuter extends BasePDFActionExecuter 
+{
 
 	/**
      * The logger
      */
-    private static Log         logger                   = LogFactory.getLog(PDFConvertToArchivableActionExecuter.class);
+    private static Log         logger                   				  = LogFactory.getLog(PDFConvertToArchivableActionExecuter.class);
 
     /**
      * Action constants
      */
-    public static final String NAME                     = "pdf-archive";
-    public static final String PARAM_DESTINATION_FOLDER = "destination-folder";
-    public static final String PARAM_DESTINATION_NAME   = "destination-name";
-    public static final String PARAM_ARCHIVE_LEVEL		= "archive-level";
+    public static final String NAME                     				  = "pdf-archive";
+    public static final String PARAM_DESTINATION_FOLDER 				  = "destination-folder";
+    public static final String PARAM_ARCHIVE_LEVEL						  = "archive-level";
 
-
+    /**
+     * Constraints
+     */
+    public static HashMap<String, String> archiveLevelConstraint          = new HashMap<String, String>();
+    
     /**
      * Add parameter definitions
      */
@@ -33,13 +43,14 @@ public class PDFConvertToArchivableActionExecuter extends BasePDFActionExecuter 
     protected void addParameterDefinitions(List<ParameterDefinition> paramList)
     {
         paramList.add(new ParameterDefinitionImpl(PARAM_DESTINATION_FOLDER, DataTypeDefinition.NODE_REF, false, getParamDisplayLabel(PARAM_DESTINATION_FOLDER)));
-        paramList.add(new ParameterDefinitionImpl(PARAM_DESTINATION_NAME, DataTypeDefinition.TEXT, false, getParamDisplayLabel(PARAM_DESTINATION_NAME)));
-        
+        paramList.add(new ParameterDefinitionImpl(PARAM_ARCHIVE_LEVEL, DataTypeDefinition.INT, true, getParamDisplayLabel(PARAM_ARCHIVE_LEVEL), false, "pdfc-archivelevel"));
+
         super.addParameterDefinitions(paramList);
     }
     
 	@Override
-	protected void executeImpl(Action action, NodeRef actionedUponNodeRef) {
+	protected void executeImpl(Action action, NodeRef actionedUponNodeRef) 
+	{
 		
 		// get a connection to OpenOffice via the usual Alfresco means
 		
@@ -49,4 +60,26 @@ public class PDFConvertToArchivableActionExecuter extends BasePDFActionExecuter 
 
 	}
 
+	/**
+	* Returns DocumentFormat of PDF/A
+	*/
+	/*private DocumentFormat toDocumentFormatPDFA(int level) {
+
+	    DocumentFormat customPdfFormat = new DocumentFormat(PORTABEL_FORMAT, PDF_APP, "pdf");
+	    customPdfFormat.setExportFilter(DocumentFamily.TEXT, "writer_pdf_Export");
+	    final Map<String, Integer> pdfOptions = new HashMap<String, Integer>();
+	    pdfOptions.put("SelectPdfVersion", level);
+	    customPdfFormat.setExportOption(DocumentFamily.TEXT, "FilterData", pdfOptions);
+	    return customPdfFormat;
+	}*/
+	
+    /**
+     * Setter for constraint bean
+     * 
+     * @param encryptionLevelConstraint
+     */
+    public void setArchiveLevelConstraint(MapConstraint mc)
+    {
+        archiveLevelConstraint.putAll(mc.getAllowableValues());
+    }
 }
