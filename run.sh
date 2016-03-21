ed80e9296135b832f9c573b7c21af7310a209788
Nathan McMinn <nathan.mcminn@alfresco.com>
2016-03-21 09:43:13 -0500

    Updated to SDK 2.2 / Alfresco 5.1

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [[ -z ${MAVEN_OPTS} ]]; then
+    echo "The environment variable 'MAVEN_OPTS' is not set, setting it for you";
+    MAVEN_OPTS="-Xms256m -Xmx1524m -XX:PermSize=300m"
+fi
+echo "MAVEN_OPTS is set to '$MAVEN_OPTS'";
+mvn clean install -Prun
